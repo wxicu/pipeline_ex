2c6449d405c011761207dcc84312ea3f4b10078f
wxicu <xichenwu@outlook.com>
2022-04-18 23:09:12 +0200

    update vireo, rscript

--- analysis_demuxlet.R
+++ analysis_demuxlet.R
@@ -1,50 +1,67 @@
-library(R.utils)
-library(ggplot2);
-library(data.table);
-args = commandArgs(trailingOnly=TRUE)
+#!/usr/bin/env Rscript
+suppressPackageStartupMessages(library("argparse"))
+suppressPackageStartupMessages(library("data.table"))
+suppressPackageStartupMessages(library("ggplot2"))
 
-if (length(args)==0) {
-  stop("At least one argument must be supplied (input file).n", call.=FALSE)
+
+#create parser object
+parser <- ArgumentParser()
+# specify desired options 
+#didnt decide whether to allow only one output file or multiple
+#here just take one single result as input
+
+parser$add_argument("-t", "--tool", default="demuxlet", type="String", help="demultiplexing tool [default %(default)s]")
+parser$add_argument("file", nargs=1, help="demultiplexing output file")
+args <- parser$parse_args()
+file <- args$file 
+if( file.access(file) == -1) {
+  stop(sprintf("Specified file ( %s ) does not exist", file))
 } 
-#else if (length(args)==1) {
-  # default output file
- # args[2] = "out.txt"
-#}
+if( args$tool == "demuxlet") {
+  demux = fread(file)
+  demux_df = readDemux(demux)
+  num_stat(demux_df)
+  
+  
+  #violin plot of log likelihood
+  vlplot <- ggplot(demux_df, aes(cell.type, BEST.LLK, color = cell.type)) + geom_violin() + geom_boxplot(width=0.1, position = position_dodge(0.9)) + ylab("log likelihood")+ xlab("different cell types")
+  png("violinplot.png")
+  print(vlplot)
+  while (!is.null(dev.list()))  dev.off()
+} 
 
-demux <- fread(args[1]);
-
-demux_df <- demux[,c(2,5,6,7)]
-demux_df$cell.type  <- sapply(demux_df$BEST.GUESS,function(x){
-  splitlist = strsplit(x,",")[[1]]
-  if (splitlist[[1]] == splitlist[[2]]){ 
-    splitlist[[2]]}
-  else{
-    "DBL"}
-})
-
-#violin plot of log likelihood
-vlplot <- ggplot(demux_df, aes(cell.type, BEST.LLK, color = cell.type)) + geom_violin() + geom_boxplot(width=0.1, position = position_dodge(0.9)) + ylab("log likelihood")+ xlab("different cell types")
-png("violinplot.png")
-print(vlplot)
-while (!is.null(dev.list()))  dev.off()
-
-
-# number statistics
-num_drop = nrow(demux_df)
-print(paste("number of droplets: ", num_drop ))
-num_sin = nrow(demux_df[demux_df$DROPLET.TYPE=="SNG",])
-prop_sin = round(num_sin*100/num_drop,2)
-print(paste("number of singlets: ", num_sin, ", with proportion ",prop_sin ,"%"))
-num_dou = nrow(demux_df[demux_df$DROPLET.TYPE=="DBL",])
-prop_dou = round(num_dou*100/num_drop,2)
-print(paste("number of doublets: ", num_dou, ", with proportion ", prop_dou,"%"))
-celltyple = unique(demux_df$cell.type)
-for( c in celltyple ){
-  if (c!="DBL"){
-    num_cell = nrow(demux_df[demux_df$cell.type==c,])
-    prop_cell =  round(num_cell*100/num_drop,2)
-    print(paste("Among the singlets, number of ", c , ": ", num_cell, ", with proportion ", prop_cell,"%"))
-    
-  }
+readDemux <- function(demux){
+  demux_df <- demux[,c(2,5,6,7)]
+  demux_df$cell.type  <- sapply(demux_df$BEST.GUESS,function(x){
+    splitlist = strsplit(x,",")[[1]]
+    if (splitlist[[1]] == splitlist[[2]]){ 
+      splitlist[[2]]}
+    else{
+      "DBL"}
+  })
+  return (demux_df)
 }
+
+
+num_stat <- function(demux_df){
+  # number statistics
+  num_drop = nrow(demux_df)
+  print(paste("number of droplets: ", num_drop ))
+  num_sin = nrow(demux_df[demux_df$DROPLET.TYPE=="SNG",])
+  prop_sin = round(num_sin*100/num_drop,2)
+  print(paste("number of singlets: ", num_sin, ", with proportion ",prop_sin ,"%"))
+  num_dou = nrow(demux_df[demux_df$DROPLET.TYPE=="DBL",])
+  prop_dou = round(num_dou*100/num_drop,2)
+  print(paste("number of doublets: ", num_dou, ", with proportion ", prop_dou,"%"))
+  celltyple = unique(demux_df$cell.type)
+  for( c in celltyple ){
+    if (c!="DBL"){
+      num_cell = nrow(demux_df[demux_df$cell.type==c,])
+      prop_cell =  round(num_cell*100/num_drop,2)
+      print(paste("Among the singlets, number of ", c , ": ", num_cell, ", with proportion ", prop_cell,"%"))
+    }
+  }
+  
+}
+
 
--- main.nf
+++ main.nf
@@ -5,22 +5,36 @@ nextflow.enable.dsl=2
 /*  
     Usage:
 	nextflow run main.nf 
+
+    Global parameter:
+	--outdir <path>
+	--demux_tool <demuxlet or souporcell or vireo>
     Available parameter for demuxlet v2:
-	--bam <bam_file> 
+	--bam <bam_file>
 	--vcf <vcf_file>
 	--field <GT or GP or PL>
-	--demux_tool <demuxlet or souporcell>
-        --outdir <path>
+	
+    Available paramter for souporcell:
+	--bam <bam_file>
+	--barcode <barcode tsv>
+        --ref <reference fasta>
+        --t <thread>
+	--k <cluster>
 
-container "${ params.demux_tool == 'demuxlet' ? 'popscle' : 'sourporcell' }"
+   Available paramter for vireo:
+	--celldata <celldata vcf>
+	--ndonor <number of donor>
+        --donorfile <donor vcf>
+        --forceLearnGT <True or False>
+	
+  
 
 */
 
 
 process demuxlet{
-
     publishDir "$params.outdir/demux", mode: 'copy'
-    container 'conda'
+    container 'popscle'
 
     input:
 	path input_bam
@@ -28,16 +42,70 @@ process demuxlet{
 	val input_field
 
     output:
-	file 'demux_out*'
+	file 'demux*'
 
     script:
     
     """
-    popscle demuxlet --sam ${input_bam} --vcf ${input_vcf} --field ${input_field} --out demux_out
+    popscle demuxlet --sam ${input_bam} --vcf ${input_vcf} --field ${input_field} --out demux
+    """  
+}
+
+process souporcell{
+
+    publishDir "$params.outdir/souporcell", mode: 'copy'
+    echo true
+
+    input:
+	path input_bam
+	path input_barcode
+	path input_ref
+	val input_thread
+	val input_cluster
+
+    output:
+	stdout
+
+    script:
+    
+    """
+    singularity exec souporcell_latest.sif souporcell_pipeline.py -i ${input_bam} -b ${input_barcode} -f ${input_ref} -t ${input_thread} -o souporcell -k ${input_cluster}
+ 
     """  
 }
 
 
+process vireo{
+    publishDir "$params.outdir/vireo", mode: 'copy'
+    echo true 
+    /*
+    container "vireo"
+    */
+
+    input:
+	path input_celldata
+	val input_ndonor
+	path input_donorfile
+	val input_forceLearnGT
+
+    output:
+	path "vireo*"
+	
+    
+
+    script:
+    def donor = input_donorfile.name != 'NO_FILE' ? "-d $input_donorfile" : ''
+    def out = input_donorfile.name != 'NO_FILE' ? "noGT" : 'withGT'
+    def learnGT = input_forceLearnGT != 'False' ? "--forceLearnGT" : ''
+
+    """
+    vireo -c ${input_celldata} -N ${input_ndonor} -o vireo_${out} ${donor} ${learnGT}
+    """
+
+
+
+
+}
 
 process ana_demuxlet{
 
@@ -47,19 +115,20 @@ process ana_demuxlet{
     input:
 	file demux_out
     output:
-	file "violinplot.png"
+	file "violinplot_demux.png"
 	stdout
 
     script:
     """
-    Rscript $projectDir/analysis_demuxlet.R $demux_out
+    analysis_demuxlet.R -t demuxlet $demux_out
     """
-
-
 
 }
 
+
+
 workflow demultiplex{
+    
 
     if (params.demux_tool == "demuxlet"){
 	    input_bam = Channel.fromPath(params.bam)
@@ -69,19 +138,27 @@ workflow demultiplex{
 	    demuxlet(input_bam,input_vcf,input_field)
 	    ana_demuxlet(demuxlet.out)
 	
-	    
-	    
-    	    
    }
   
-   else if (params.demux_tool == "sourporcell"){
+   else if (params.demux_tool == "souporcell"){
+           input_bam = Channel.fromPath(params.bam)
+	   input_barcode = Channel.fromPath(params.barcode)
+	   input_ref = Channel.fromPath(params.ref)
+	   input_thread = channel.value(params.t)
+	   input_cluster = channel.value(params.k)
+ 	   
+	   souporcell(input_bam, input_barcode, input_ref, input_thread, input_cluster)	 
 
-
-
+}
+   else if (params.demux_tool == "vireo"){
+	   input_celldata = Channel.fromPath(params.celldata)
+	   input_ndonor = channel.value(params.ndonor)
+	   input_donorfile = Channel.fromPath(params.donorfile)
+	   input_forceLearnGT = channel.value(params.forceLearnGT)
+	   vireo(input_celldata,input_ndonor,input_donorfile,input_forceLearnGT)
 
 
 }
-   
     	
 }
 
